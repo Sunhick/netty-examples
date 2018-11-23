dbc83e208b7825538fcc013f37bca2d145703e34
Sunil <sunhick@gmail.com>
2018-11-23 12:32:01 -0800

    Add apt for intellij to recognize dagger2 generated src

--- src/ex01/src/main/java/com/local/Starter.java
+++ src/ex01/src/main/java/com/local/Starter.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 public class Starter {
     public static void main(String[] args) throws InterruptedException {
         log.debug("In main netty-in-action tutorials");
+
         EchoServerComponent component = DaggerEchoServerComponent.create();
         EchoServer echoServer = component.createEchoServer();
         echoServer.start();
