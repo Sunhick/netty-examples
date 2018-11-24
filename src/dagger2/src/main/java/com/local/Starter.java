ce01e2713067b86cf2d56296745158a607ea3887
Sunil <sunhick@gmail.com>
2018-11-23 23:10:58 -0800

    code clean up

--- src/dagger2/src/main/java/com/local/Starter.java
+++ src/dagger2/src/main/java/com/local/Starter.java
@@ -5,10 +5,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class Starter {
 
-    public static void main(String[] args) throws InterruptedException {
-        log.debug("In main netty-in-action tutorials");
-        // new EchoServerApplication().run();
-
+    public static void main(String[] args) {
         FooComponent fooComponent = DaggerFooComponent.create();
 
         // Not possible to print the address of the object in java.
