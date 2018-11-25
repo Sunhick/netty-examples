e74271df685e2cdbbfdd27baf2ce4d5e7cbc97e6
Sunil <sunhick@gmail.com>
2018-11-25 12:16:43 -0800

    addign ex02 barebones

---
+++ src/ex02/src/main/java/com/local/Starter.java
@@ -0,0 +1,11 @@
+package com.local;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Starter {
+    public static void main(String[] args) {
+        log.debug("In main netty-in-action tutorials -- Pipelines");
+
+    }
+}
