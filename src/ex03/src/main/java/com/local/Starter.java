000d37319e7cabdbfe10b626963430c0ff7d6ddc
Sunil <sunhick@gmail.com>
2018-11-26 00:39:05 -0800

    clean up code and log4j2

--- src/ex03/src/main/java/com/local/Starter.java
+++ src/ex03/src/main/java/com/local/Starter.java
@@ -1,7 +1,6 @@
 package com.local;
 
 import lombok.extern.log4j.Log4j2;
-import org.local.Tester;
 
 @Log4j2
 public class Starter {
@@ -13,8 +12,5 @@ public class Starter {
         log.error("Welcome to netty ex03 example.");
         log.fatal("Welcome to netty ex03 example.");
         log.trace("Welcome to netty ex03 example.");
-
-        Tester t = new Tester();
-        t.moreLog();
     }
 }
