9144f3ff2d3b8c9b574547b4b8f72e6014a3fca6
Sunil <sunhick@gmail.com>
2018-11-26 20:18:50 -0800

    fix curlws script

--- src/ex04/curlws.sh
+++ src/ex04/curlws.sh
@@ -2,8 +2,8 @@ curl --include \
      --no-buffer \
      --header "Connection: Upgrade" \
      --header "Upgrade: websocket" \
-     --header "Host: example.com:80" \
-     --header "Origin: http://example.com:80" \
+     --header "Host: localhost:9999" \
+     --header "Origin: http://localhost:9999" \
      --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
      --header "Sec-WebSocket-Version: 13" \
      http://localhost:9999/ws/ping -v
