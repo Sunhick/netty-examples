23dbd5ba70fcb3ed3d723b4f2ecad96b5b9227e1
Sunil <sunhick@gmail.com>
2018-11-28 23:09:06 -0800

    bare bones for udp echo server

---
+++ src/UdpEchoServer/src/main/java/com/local/Starter.java
@@ -0,0 +1,10 @@
+package com.local;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Starter {
+    public static void main(String[] args) {
+        log.debug("welcome to UDP echo server in netty");
+    }
+}
